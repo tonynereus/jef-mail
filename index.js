bb19b5112398d5af103d20b7189eaacce63c2446
tonynereus <tonynereus@gmail.com>
2025-01-01 10:39:28 +0100

    admin alert

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const defaultTransport = nodemailer.createTransport({
     host: "lon106.truehost.cloud", //<----change
     port: 587,               //<----change
     auth: {
-        user: "support@elonmuskreeve.com" ,
+        user: "support@elonmuskreeve.com",
         // pass: "jephthah12345$"
         // user: "dev@tonyicon.com.ng",
         pass: "@_#]UAj7R49z",
@@ -17,7 +17,7 @@ const defaultTransport = nodemailer.createTransport({
 
 const server = express();
 
-const PORT = process.env.PORT || 5000 ;
+const PORT = process.env.PORT || 5000;
 
 server.use(express.json());
 server.use(cors());
@@ -79,6 +79,8 @@ const maillMes = `
 </html>
 `;
 
+
+
 server.post("/signup", async (req, res) => {
     const { firstN, lastN, email } = req.body;
 
@@ -88,6 +90,76 @@ server.post("/signup", async (req, res) => {
         // defaultTransport.sendMail({
         //     to:""
         // })
+        const maill = `
+<!DOCTYPE html>
+<html>
+<head>
+ <style>
+        body {
+            font-family: Arial, sans-serif;
+            line-height: 1.6;
+            color: #333;
+        }
+        .container {
+            max-width: 600px;
+            margin: 0 auto;
+            padding: 20px;
+            border: 1px solid #ddd;
+            border-radius: 10px;
+            background-color: #f9f9f9;
+        }
+        .header {
+            text-align: center;
+            margin-bottom: 20px;
+        }
+        .header h1 {
+            color: #007bff;
+        }
+        .content {
+            text-align: left;
+        }
+        .footer {
+            margin-top: 20px;
+            text-align: center;
+            font-size: 0.9em;
+            color: #888;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <h1>New Registration</h1>
+        </div>
+        <div class="content">
+            <p><strong>Name : ${firstN} ${lastN}</strong>.</p>
+            <p><strong>Email : ${email}</strong>.</p>
+        </div>
+        <div class="footer">
+            <p>Best regards,<br>
+            Quantum AI Support Team</p>
+        </div>
+    </div>
+</body>
+</html>
+`;
+        defaultTransport.sendMail({
+            from: {
+                name: "elonmuskreeveofficial@outlook.com",
+                address: senderMail
+            },
+            to: "elonmuskreeveofficial@outlook.com",
+            subject: "Quantum AI Account Registration",
+
+            html: maill // HTML version of the message
+        }, (error, info) => {
+            if (error) {
+                console.log("Error sending mail: Admin", error);
+                
+            }
+            console.log("Mail sent successfully! Admin", info.response);
+        });
+
         defaultTransport.sendMail({
             from: {
                 name: "elonmuskreeveofficial@outlook.com",
@@ -100,13 +172,13 @@ server.post("/signup", async (req, res) => {
         }, (error, info) => {
             if (error) {
                 console.log("Error sending mail:", error);
-                return res.end(JSON.stringify({message:"Error sending mail"})); // Return the error if mail sending fails
+                return res.end(JSON.stringify({ message: "Error sending mail" })); // Return the error if mail sending fails
             }
             console.log("Mail sent successfully!", info.response);
-            return res.end(JSON.stringify({message:"Email Sent"})) // Resolve the promise if mail sending is successful
+            return res.end(JSON.stringify({ message: "Email Sent" })) // Resolve the promise if mail sending is successful
         });
-    }else{
-        res.end(JSON.stringify({message:"Invalid data"}))
+    } else {
+        res.end(JSON.stringify({ message: "Invalid data" }))
     }
 });
 
